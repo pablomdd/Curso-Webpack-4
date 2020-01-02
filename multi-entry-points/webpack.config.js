a593eac0b503ab700ef0fbb6f13774dc0d9a4ffb
pablomdd <pablo.mdd.007@gmail.com>
2020-01-01 22:30:45 -0600

    Added: Multiple Entry Points

---
+++ multi-entry-points/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        home: path.resolve(__dirname,'src/js/index.js'),
+        prices: path.resolve(__dirname,'src/js/prices.js'),
+        contact: path.resolve(__dirname,'src/js/contact.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].js',
+    }
+}
