b4fa4d49b18955e96faab55e67bc334166fe04c2
pablomdd <pablo.mdd.007@gmail.com>
2020-01-02 21:52:31 -0600

    Added modern js example and runtime plugin and plugin-transform-runtime plugin

--- babel/src/js/index.js
+++ babel/src/js/index.js
@@ -1,11 +1,24 @@
 import '../css/index.css'
-import text from './text'
+import '../js/search'
+import search from '../js/search'
+import render from '../js/render'
+// import text from './text'
 
-text()
+// text()
 
-if(module.hot){
-    module.hot.accept('./text.js',function(){
-        console.log('Recargardo en caliente')
-        text()
+// if(module.hot){
+//     module.hot.accept('./text.js',function(){
+//         console.log('Recargardo en caliente')
+//         text()
+//     })
+// }
+
+const id = prompt('Quién es ese pokemon?')
+
+search(id)
+    .then((data) => {
+        render(data)
     })
-}
+    .catch(() => {
+        console.log('No se pudo cargar el pokemon')
+    })
