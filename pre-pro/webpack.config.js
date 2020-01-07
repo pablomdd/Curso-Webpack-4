c825954050ae89236b626fcbd12b01bccd0aaa3b
pablomdd <pablo.mdd.007@gmail.com>
2020-01-06 20:52:23 -0600

    Added Prepro capcities for sass, less and post css

---
+++ pre-pro/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        home: path.resolve(__dirname,'src/js/index.js'),
+     },
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].js',
+    },
+    devServer:{
+        hot: true,
+        open: true,
+        port: 9000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jpg|png|gif|eot|ttf|svg|mp4|webm$/,
+                use: {
+                    loader: 'url-loader',
+                    options:{
+                        limit: 90000,
+                    }
+                }
+                
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader', 
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".jsx"]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'webpack-dev-server',
+            template: path.resolve(__dirname,'index.html')
+        }),
+    ]
+}
